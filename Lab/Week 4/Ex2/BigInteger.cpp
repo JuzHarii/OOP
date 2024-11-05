9634774ed6ce5acd7d573a9199cdea7447fa0c7b
JuzHarii <139064128+JuzHarii@users.noreply.github.com>
2024-11-05 10:54:04 +0700

    add and sub function
    switched to doubly linked list

--- Lab/Week 4/Ex2/BigInteger.cpp
+++ Lab/Week 4/Ex2/BigInteger.cpp
@@ -2,52 +2,106 @@
 
 // ! display
 ostream& operator<<(ostream& os, const BigInteger& bi){
-    LinkedList list_temp = bi.list;
-    Node* ptemp = list_temp.getHead();
+    Node* ptemp = bi.list.getHead();
     while(ptemp){
         os << ptemp->data;
         ptemp = ptemp->next;
     }
     return os;
 }
-// ! consttructors
-BigInteger::BigInteger() : list(0){}
+
+// ! constructors
+BigInteger::BigInteger() : list(0) {}
 
 BigInteger::BigInteger(long long n){
-    while(n){
-        list.addHead(n % 10);
-        n /= 10;
+    if (n == 0) {
+        list.addHead(0);
+    } else {
+        while(n) {
+            list.addHead(n % 10);
+            n /= 10;
+        }
     }
 }
 
 BigInteger::BigInteger(int x, int n){
-    while(n--){
+    while(n--) {
         list.addHead(x);
     }
 }
 
+// ! addition operator
 BigInteger BigInteger::operator+(const BigInteger& bi) {
     LinkedList list_temp = bi.list;
-    LinkedList list_res; // Result of the addition
+    LinkedList list_res;
 
-    Node* ptemp1 = list.getHead(); // Pointer to traverse the current BigInteger's list
-    Node* ptemp2 = list_temp.getHead(); // Pointer to traverse the other BigInteger's list
+    Node* ptemp1 = list.getTail();
+    Node* ptemp2 = list_temp.getTail();
 
     int carry = 0;
-    while (ptemp1 || ptemp2) {
-        int sum = carry + (ptemp1 ? ptemp1->data : 0) + (ptemp2 ? ptemp2->data : 0);
-        carry = sum / 10; // if sum >= 10, carry = 1, else carry = 0
-        list_res.addHead(sum % 10); // Add each result digit to the head for reversed order
-        
-        if (ptemp1) ptemp1 = ptemp1->next;
-        if (ptemp2) ptemp2 = ptemp2->next;
+    while (ptemp1 || ptemp2 || carry) {
+        int sum = (ptemp1 ? ptemp1->data : 0) + (ptemp2 ? ptemp2->data : 0) + carry;
+
+        list_res.addHead(sum % 10);
+        carry = sum / 10;
+
+        if (ptemp1) ptemp1 = ptemp1->prev;
+        if (ptemp2) ptemp2 = ptemp2->prev;
     }
 
-    if (carry) {
-        list_res.addHead(carry); // Add final carry at the head, if any
+    BigInteger result;
+    result.list = list_res;
+    return result;
+}
+
+// ! subtraction operator
+BigInteger BigInteger::operator-(const BigInteger& bi) {
+    LinkedList list_temp = bi.list;
+    LinkedList list_res;
+
+    Node* ptemp1 = list.getTail();
+    Node* ptemp2 = list_temp.getTail();
+
+    int carry = 0;
+    while (ptemp1 || ptemp2 || carry) {
+        int digit1 = (ptemp1) ? ptemp1->data : 0;
+        int digit2 = (ptemp2) ? ptemp2->data : 0;
+
+        int sub = digit1 - digit2 - carry;
+
+        // If the result is negative, borrow from the next place
+        if (sub < 0) {
+            sub += 10;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+
+        list_res.addHead(sub);
+
+        // Move to the previous nodes in each list
+        if (ptemp1) ptemp1 = ptemp1->prev;
+        if (ptemp2) ptemp2 = ptemp2->prev;
     }
 
-    BigInteger res;
-    res.list = list_res;
-    return res;
+    // Remove any leading zeros from the result list
+    while (list_res.getHead() && list_res.getHead()->data == 0 && list_res.getHead() != list_res.getTail()) {
+        list_res.removeHead();
+    }
+
+    BigInteger result;
+    result.list = list_res;
+    return result;
+}
+
+// ! Addition with long long
+BigInteger operator+(long long n, const BigInteger& bi){
+    BigInteger temp(n);
+    return temp + bi;
+}
+
+// ! Subtraction with long long
+BigInteger operator-(long long n, const BigInteger& bi){
+    BigInteger temp(n);
+    return temp - bi;
 }
--- Lab/Week 4/Ex2/Linked_List.cpp
+++ Lab/Week 4/Ex2/Linked_List.cpp
@@ -2,46 +2,56 @@
 
 using namespace std;
 
-struct Node{
+struct Node {
     int data;
     Node* next;
+    Node* prev;  // Added prev pointer for doubly linked list
 };
 
-class LinkedList{
+class LinkedList {
 private:
-    Node *pHead, *pTail;
+    Node* pHead;
+    Node* pTail;
     int curN;
+
 public:
-    LinkedList(){
+    LinkedList() {
         pHead = nullptr;
         pTail = nullptr;
+        curN = 0;
     }
-    LinkedList(int x){
+
+    LinkedList(int x) {
         curN = 1;
         pHead = pTail = createNode(x);
     }
-    ~LinkedList(){
-        while(pHead){
+
+    ~LinkedList() {
+        while (pHead) {
             Node* temp = pHead;
             pHead = pHead->next;
             delete temp;
         }
     }
-    static Node* createNode(const int& val){
+
+    static Node* createNode(const int& val) {
         Node* temp = new Node;
         temp->data = val;
         temp->next = nullptr;
+        temp->prev = nullptr;  // Initialize prev to nullptr
         return temp;
     }
+
     void addHead(const int& n) {
         Node* temp = createNode(n);
         if (!pHead) {
             pHead = pTail = temp;
         } else {
             temp->next = pHead;
+            pHead->prev = temp;
             pHead = temp;
         }
-        curN++; // Increase the count
+        curN++;  // Increase the count
     }
 
     void addTail(const int& n) {
@@ -50,43 +60,44 @@ public:
             pHead = pTail = temp;
         } else {
             pTail->next = temp;
+            temp->prev = pTail;
             pTail = temp;
         }
-        curN++; // Increase the count
+        curN++;  // Increase the count
     }
 
     Node* removeHead() {
         if (!pHead) return nullptr;
         Node* temp = pHead;
         pHead = pHead->next;
+        if (pHead) pHead->prev = nullptr;
+        else pTail = nullptr;
         delete temp;
-        curN--; // Decrease the count
-        if (!pHead) pTail = nullptr;
+        curN--;  // Decrease the count
         return pHead;
     }
 
     Node* removeTail() {
         if (!pHead) return nullptr;
-        Node* temp = pHead;
+        Node* temp = pTail;
         if (pHead == pTail) {
             delete pHead;
             pHead = pTail = nullptr;
             curN = 0;
             return nullptr;
         }
-        while (temp->next != pTail) {
-            temp = temp->next;
-        }
-        delete pTail;
-        pTail = temp;
+        pTail = pTail->prev;
         pTail->next = nullptr;
-        curN--; // Decrease the count
+        delete temp;
+        curN--;
         return pTail;
     }
-    Node* getHead(){
+
+    Node* getHead() {
         return pHead;
     }
-    Node* getTail(){
+
+    Node* getTail() {
         return pTail;
     }
 };
